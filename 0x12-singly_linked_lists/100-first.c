b18aae8509c49e3652657b8e64c5424ad6decc34
MahmoudZak3 <88795478+MahmoudZak3@users.noreply.github.com>
2023-07-28 02:10:21 +0300

    zak com

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,17 @@
 /*
- * File: 100-first.c
- * Auth: Firdaus H. Salim
+ * Auth: Mod Zak
  */
 
 #include <stdio.h>
 
-void __attribute__((constructor)) hare(void);
+void first(void) __attribute__ ((constructor));
 
 /**
- * hare - Prints a string before the
- *        main function is executed.
+ * first - prints a string before the main
+ * function is executed
  */
-void hare(void)
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
